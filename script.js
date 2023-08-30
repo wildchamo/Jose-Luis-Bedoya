8cdc790a7e7a9b71f2ee6e7033f6d9f837450786
Jose Luis <wildchamo@gmail.com>
2023-08-29 23:37:42 -0500

    english support for the webpage

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    const body=document.querySelector('body');
+const userLanguage = navigator.language;
+const aboutme= document.querySelector('#aboutme');
+const contact= document.querySelector('#contacto');
+const marquee= document.querySelector('#marquee');
 
-    
-    // El usuario tiene habilitado el modo oscuro
-    console.log('Modo oscuro habilitado');
-    
-} else {
-    // El usuario no tiene habilitado el modo oscuro
-    console.log('Modo oscuro no habilitado');
+console.log('Idioma del usuario:', userLanguage);
+
+if(userLanguage ==="en"){
+    marquee.innerText= 'HELLO, WELCOME TO THE WEB PAGE OF THE INTERNET ASTRO AND THUG @WILDCHAMO. HERE GOES A POEM BY JOHN DONNE: NO MAN IS AN ILAND, INTIRE OF IT SELFE; EVERY MAN IS A PEACE OF THE CONTINENT, A PART OF THE MAINE; IF A CLOD BEE WASHED AWAY BY THE SEA, EUROPE IS THE LESSE, AS WELL AS IF A PROMONTORIE WERE, AS WELL AS IF A MANNOR OF THY FRIENDS OR OF THINE OWNE WERE; ANY MANS DEATH DIMINISHES ME, BECAUSE I AM INVOLVED IN MANKINDE; AND THEREFORE NEVER SEND TO KNOW FOR WHOM THE BELL TOLLS; IT TOLLS FOR THEE...';
+    aboutme.innerHTML = '  <img src="user.png" class="iconuser" alt="" />About me';
+    contact.innerHTML = '<img src="mail.png" class="iconuser" alt="" /> Contact';
 }
